bd2f46acbbc006f10174afdef233644b4f19ad66
Tara Hensle <tara@northwestern.edu>
2017-02-19 19:08:41 -0600

    edits

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 //Game State Initializer and storing variables
 
+
 var blueGemValue = crystalValue();
 var greenGemValue = crystalValue();
 var orangeGemValue = crystalValue();
@@ -19,9 +20,31 @@ var crystalCollector = {
 	$(".losses").html(crystalCollector.lossCounter);
 	$(".target").html(crystalCollector.targetScore);
 
+//Check if crystal buttons have the same value
+
+
+
 //Event Listeners for crystal buttons
 
+$(".blue-gem").on("click", function(){
+	addingCrystalValue(blueGemValue);
+	winOrLose();
+});
 
+$(".green-gem").on("click", function(){
+	addingCrystalValue(greenGemValue);
+	winOrLose();
+});
+
+$(".orange-gem").on("click", function(){
+	addingCrystalValue(orangeGemValue);
+	winOrLose();
+});
+
+$(".yellow-gem").on("click", function(){
+	addingCrystalValue(yellowGemValue);
+	winOrLose();
+});
 
 //FUNCTIONS
 
@@ -42,5 +65,51 @@ function crystalValue(){
 	return number;
 };
 
+//Adding crystal values to the user score
+
+function addingCrystalValue(number){
+	var result = number + crystalCollector.userScore;
+	crystalCollector.userScore = result;
+	$(".score").html(crystalCollector.userScore);
+};
+
+//Checking if player won or lost
+
+function winOrLose(){
+
+	//Check if win
+	if (crystalCollector.userScore === crystalCollector.targetScore){
+		crystalCollector.winCounter++;
+		reset();
+		alert("Congrats, you've won!");
+	};
+
+	//Check if lose
+	if (crystalCollector.userScore > crystalCollector.targetScore){
+		crystalCollector.lossCounter++;
+		reset();
+		alert("Oh no! Try again!");
+	};
+
+};
+
+//Use "gameDone" variable to reset board (targetScore, crystal values, user score)
+
+function reset(){
+
+	//Reset user and target numbers, randomize new crystal values
+	crystalCollector.userScore = 0;
+	crystalCollector.targetScore = targetNumber();
+	blueGemValue = crystalValue();
+	greenGemValue = crystalValue();
+	orangeGemValue = crystalValue();
+	yellowGemValue = crystalValue();
+
+	//Display this for user
+	$(".wins").html(crystalCollector.winCounter);
+	$(".losses").html(crystalCollector.lossCounter);
+	$(".target").html(crystalCollector.targetScore);
+	$(".score").empty();
+};
 
 });
